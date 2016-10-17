020473e771713da607805ef442ea197646ac9770
Joakim Landström <joakim.landstrom@outlook.com>
2016-10-17 14:26:32 +0200

    entity manager

--- src/main/java/se/jola/repo/UserRepository.java
+++ src/main/java/se/jola/repo/UserRepository.java
@@ -1,5 +1,32 @@
 package se.jola.repo;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import se.jola.entities.User;
+
 public class UserRepository {
+
+    private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("PersistenceUnit");
+
+    private EntityManager manager;
+
+    private EntityManager getManager() {
+	return FACTORY.createEntityManager();
+    }
+
+    public void saveUser(User user) {
+
+	manager = getManager();
+
+	manager.getTransaction();
+
+	manager.persist(user);
+
+	manager.getTransaction().commit();
+
+	manager.close();
+    }
 
 }
